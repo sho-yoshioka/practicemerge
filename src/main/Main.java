941a2193109b92928fdc50793b54e10219a1b259
sho-yoshioka <120555627+sho-yoshioka@users.noreply.github.com>
2022-12-17 14:08:28 +0900

    feature2で作業しました

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Practice Git!");
 		System.out.println("plugin Git connect local");
+		System.out.println("feature2で作業しました");
 	}
 
 }
